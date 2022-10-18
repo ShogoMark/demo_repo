b4129320f8634ff2759dee10102f576c0e173789
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-18 22:09:58 +0000

    main.h and _printf

--- main.h
+++ main.h
@@ -19,11 +19,11 @@
 #define S_SHORT 1
 
 /**
- *  * struct fmt - Struct op
- *   *
- *    * @fmt: The format.
- *     * @fn: The function associated.
- *      */
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
 
 struct fmt
 {
@@ -33,11 +33,11 @@ struct fmt
 
 
 /**
- *  * typedef struct fmt fmt_t - Struct op
- *   *
- *    * @fmt: The format.
- *     * @fm_t: The function associated.
- *      */
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
 
 typedef struct fmt fmt_t;
 
